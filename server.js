369beb9daa79e73963e9e669b780f054c2209d35
Weslley Braga <wesbragagt@gmail.com>
2019-04-29 15:22:53 -0500

    working burger app

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var PORT = process.env.PORT || 3000;
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
+
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
