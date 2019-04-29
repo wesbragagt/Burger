c948944de927da3cea92d3c210a796e4e6008130
Weslley Braga <wesbragagt@gmail.com>
2019-04-29 18:26:32 -0500

    uploading database to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,19 @@
 const mysql = require("mysql");
+let connection;
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "wdlbsnmw95",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    // Database is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // Database is local
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "wdlbsnmw95",
+        database: "burgers_db"
+    });
+}
 
 // Make connection.
 connection.connect(function(err) {
